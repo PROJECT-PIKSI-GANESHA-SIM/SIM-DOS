20c6c8a31b59bb92022b26b80db912fd1c00f934
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-06-26 20:29:57 +0700

    update register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="nidn" class="col-md-4 col-form-label text-md-end">{{ __('NIDN') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nidn" type="text" class="form-control @error('nidn') is-invalid @enderror" name="nidn" value="{{ old('nidn') }}" required autocomplete="nidn" >
+
+                                @error('nidn')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
