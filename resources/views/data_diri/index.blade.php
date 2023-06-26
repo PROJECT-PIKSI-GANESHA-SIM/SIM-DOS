f619a13a66225ab6b4ba3b8c81d62f437d5d1fb2
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-06-26 21:28:25 +0700

    layouting data diri page

--- resources/views/data_diri/index.blade.php
+++ resources/views/data_diri/index.blade.php
@@ -9,57 +9,59 @@
     <div class="col-md-6">
         <div class="card shadow bg-white flex-row card-rounded">
             <div class="card-body my-auto">
-                <div class="title d-flex">
+                <div class="title d-flex align-items-center justify-content-between">
                     <span class="fw-bold fs-5">Identitas Diri</span>
-                    <img src="{{ asset("assets/edit.png") }}" alt="" width="20px" height="20px">
+                    <a href="" class="justify-content-end">
+                        <img class="" src="{{ asset("assets/edit.png") }}" alt="" width="25px" height="25px">
+                    </a>
                 </div>
                 <hr>
                 <div class="image-profile d-flex justify-content-center align-items-center">
                     <img src="{{ asset('assets/PIKSI.png') }}" alt="" width="170px;" height="170px">
                 </div>
-                <div class="items mt-5 me-2 ms-2">
-                    <div class="row">
+                <div class="items mt-5 me-5 ms-5">
+                    <div class="row mb-2">
                         <div class="col-md-6">NIDN</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">NIP</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">NIK</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Nama</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Jenis Kelamin</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Golongan Darah</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Kewarganegaraan</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Agama</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Tempat Lahir</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Tanggal Lahir</div>
                         <div class="col-md-6">sasa</div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">NIDN</div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Status Perkawinan</div>
                         <div class="col-md-6">sasa</div>
                     </div>
                 </div>
@@ -69,9 +71,58 @@
     <div class="col-md-6">
         <div class="card shadow bg-white flex-row card-rounded">
             <div class="card-body my-auto">
-                <div class="title d-flex">
-                    <span class="fw-bold fs-5">Alamat Dan Kontak</span>
-                    <hr>
+                <div class="title d-flex align-items-center justify-content-between">
+                    <span class="fw-bold fs-5">Alamat Dan Kotak</span>
+                    <a href="" class="justify-content-end">
+                        <img class="" src="{{ asset("assets/edit.png") }}" alt="" width="25px" height="25px">
+                    </a>
+                </div>
+                <hr>
+                <div class="items mt-5 me-5 ms-5">
+                    <div class="row mb-2">
+                        <div class="col-md-6">Email</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Alamat</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">RT</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">RW</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">NO</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Desa/Kelurahan</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Kota/Kabupaten</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Provinsi</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Kode Pos</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">No Telepon Rumah</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">No Handphone</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -82,9 +133,26 @@
     <div class="col-md-6">
         <div class="card shadow bg-white flex-row card-rounded">
             <div class="card-body my-auto">
-                <div class="title">
-                    <span class="fw-bold fs-5">Lain -Lain</span>
-                    <hr>
+                <div class="title d-flex align-items-center justify-content-between">
+                    <span class="fw-bold fs-5">Lain - Lain</span>
+                    <a href="" class="justify-content-end">
+                        <img class="" src="{{ asset("assets/edit.png") }}" alt="" width="25px" height="25px">
+                    </a>
+                </div>
+                <hr>
+                <div class="items mt-5 me-5 ms-5">
+                    <div class="row mb-2">
+                        <div class="col-md-6">NPWP</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Nama Wajib Pajak</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">SINTA ID</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -92,9 +160,46 @@
     <div class="col-md-6">
         <div class="card shadow bg-white flex-row card-rounded">
             <div class="card-body my-auto">
-                <div class="title">
+                <div class="title d-flex align-items-center justify-content-between">
                     <span class="fw-bold fs-5">Kepegawaian</span>
-                    <hr>
+                    <a href="" class="justify-content-end">
+                        <img class="" src="{{ asset("assets/edit.png") }}" alt="" width="25px" height="25px">
+                    </a>
+                </div>
+                <hr>
+                <div class="items mt-5 me-5 ms-5">
+                    <div class="row mb-2">
+                        <div class="col-md-6">Program Studi</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Status Kepegawaian</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Status Keaktifan</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Jabatan Fungsional</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">NO SK Sertifikasi Dosen</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Nomor SK TMMD</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Tanggal Mulai Menjadi Dosen</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">Pangkat/Golongan</div>
+                        <div class="col-md-6">sasa</div>
+                    </div>
                 </div>
             </div>
         </div>
