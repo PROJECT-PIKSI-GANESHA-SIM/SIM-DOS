82ccd62de25e7e2d52c54cad4d52b0ea42d70825
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-07-19 15:52:58 +0700

    mod navbar in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,23 +11,32 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm" style="background-color: #8A00B9">
         <div class="container">
-            <a class="navbar-brand" href="/">
-                <img src="{{ asset('assets/piksi_sim_logo.png') }}" alt="" height="50"
-                    class="d-inline-block align-text-top">
+            <a class="navbar-brand" href="#">
+                <img src="assets/PIKSI.png" alt="logo piksi" height="30" class="d-inline-block align-text-top">
+                SIM-DOS
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link" href="/">Beranda</a>
-                    <a class="nav-link" href="/dosen">Dosen</a>
-                    <a class="nav-link" href="/about">Tentang</a>
-                    <a class="nav-link" href="/contact">Kontak</a>
-                </div>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/dosen">Dosen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">Tentang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Kontak</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
