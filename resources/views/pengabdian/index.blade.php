0553261deff56d295c07c84d6154b68e4c2d0e81
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-07-23 19:58:50 +0700

    create alert for information message from role academic

--- resources/views/pengabdian/index.blade.php
+++ resources/views/pengabdian/index.blade.php
@@ -5,6 +5,10 @@
 @endsection
 
 @section('content')
+<div class="alert alert-info alert-dismissible" role="alert">
+    <div>Perubahan Data Maksimal Tanggal 23-07-2023</div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
 <div class="card shadow bg-white card-rounded">
     <div class="card-body my-auto">
         <div class="title">
@@ -121,4 +125,25 @@
         toastr.error('{{ session('error') }}', 'GAGAL!'); 
         
     @endif
+
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+    }
+
+    const alertTrigger = document.getElementById('liveAlertBtn')
+    if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('Nice, you triggered this alert message!', 'success')
+    })
+    }
+
 </script>
--- resources/views/pengajaran/index.blade.php
+++ resources/views/pengajaran/index.blade.php
@@ -5,6 +5,10 @@
 @endsection
 
 @section('content')
+<div class="alert alert-info alert-dismissible" role="alert">
+    <div>Perubahan Data Maksimal Tanggal 23-07-2023</div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
 <div class="card shadow bg-white card-rounded">
     <div class="card-body my-auto">
         <div class="title">
