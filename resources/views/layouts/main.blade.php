bd45436e2068880c037f6d9691b882668450522b
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-08-02 20:43:11 +0700

    making loader spinner

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -126,10 +126,50 @@
                 order: -1;
             }
         }
+
+        /* CSS Loader Spinner */
+        .loader {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: #333333;
+            transition: opacity 0.75s, visibility 0.75s;
+        }
+
+        .loader--hidden {
+            opacity: 0;
+            visibility: hidden;
+        }
+
+        .loader::after {
+            content: "";
+            width: 75px;
+            height: 75px;
+            border: 15px solid #dddddd;
+            border-top-color: #8A00B9;
+            border-radius: 50%;
+            animation: loading 0.75s ease infinite;
+        }
+
+        @keyframes loading {
+            from {
+                transform: rotate(0turn);
+            }
+
+            to {
+                transform: rotate(1turn);
+            }
+        }
     </style>
 </head>
 
 <body>
+    <div class="loader"></div>
     <div class="container py-5">
         @yield('container')
     </div>
@@ -173,3 +213,14 @@
 </footer>
 
 </html>
+<script>
+    window.addEventListener("load", () => {
+        const loader = document.querySelector(".loader");
+
+        loader.classList.add("loader--hidden");
+
+        loader.addEventListener("transitionend", () => {
+            document.body.removeChild(loader);
+        });
+    });
+</script>
