88ecb73123ac9d105fce77288f32fa3a424257be
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-08-05 15:18:25 +0700

    making detail dosen page

--- resources/views/detaildosen.blade.php
+++ resources/views/detaildosen.blade.php
@@ -1,3 +1,9 @@
+<style>
+    .text-end,
+    .text-center {
+        font-weight: bold;
+    }
+</style>
 @include('layouts.partials.navbar')
 
 @extends('layouts.main')
@@ -6,4 +12,178 @@
     <h3 class="text-4xl text-gray-700 mb-2 font-weight-bold py-3">
         <center>Detail Dosen</center>
     </h3>
+    <div class="container pt-4 pb-5">
+        <div class="row">
+            <div class="col-md-5 px-4">
+                <center>
+                    <img src="assets/thumbnails-profile.png" alt="logo PIKSI SIM-DOS" class="mb-4" width="380">
+                </center>
+            </div>
+            <div class="col">
+                <table class="table table-striped">
+                    <tbody>
+                        <tr>
+                            <td class="text-end" width="35%">John</td>
+                            <td>Doe</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">Mary</td>
+                            <td>Moe</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">July</td>
+                            <td>Dooley</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">July</td>
+                            <td>Dooley</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">July</td>
+                            <td>Dooley</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">July</td>
+                            <td>Dooley</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">July</td>
+                            <td>Dooley</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">July</td>
+                            <td>Dooley</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">July</td>
+                            <td>Dooley</td>
+                        </tr>
+                        <tr>
+                            <td class="text-end" width="35%">July</td>
+                            <td>Dooley</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <h3 class="text-4xl text-gray-700 mb-2 font-weight-bold py-3">Pendidikan Formal</h3>
+        <table class="table table-striped">
+            <tbody>
+                <tr class="text-center">
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+            </tbody>
+        </table>
+        <h3 class="text-4xl text-gray-700 mb-2 font-weight-bold py-3">Pengajaran</h3>
+        <table class="table table-striped">
+            <tbody>
+                <tr class="text-center">
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+            </tbody>
+        </table>
+        <h3 class="text-4xl text-gray-700 mb-2 font-weight-bold py-3">Penelitian</h3>
+        <table class="table table-striped">
+            <tbody>
+                <tr class="text-center">
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+            </tbody>
+        </table>
+        <h3 class="text-4xl text-gray-700 mb-2 font-weight-bold py-3">Pengabdian</h3>
+        <table class="table table-striped">
+            <tbody>
+                <tr class="text-center">
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+                <tr>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                    <td>Doe</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
