297fab51ec26fd7059ac68bfa469ffd4afb4e6d2
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-08-05 13:48:09 +0700

    make heading for detail dosen

--- resources/views/detaildosen.blade.php
+++ resources/views/detaildosen.blade.php
@@ -3,5 +3,7 @@
 @extends('layouts.main')
 
 @section('container')
-
+    <h3 class="text-4xl text-gray-700 mb-2 font-weight-bold py-3">
+        <center>Detail Dosen</center>
+    </h3>
 @endsection
