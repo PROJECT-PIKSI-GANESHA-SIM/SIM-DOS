4cebe1beb5f2b27f7d22eccf05e26db13db0917e
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2023-08-17 16:09:39 +0700

    create show and detail information center

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,45 +4,20 @@
     <div class="loader"></div>
     <h1 class="text-center py-5">Pusat Informasi</h1>
     <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-            <div class="card">
-                <img src="assets/thumbnails-blog.png" class="card-img-top" alt="thumbnail">
-                <div class="card-body">
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    <h5 class="card-title">
-                        <a href="">Card Title</a>
-                    </h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
+        @foreach ($pusat_informasi as $informasi)
+            <div class="col">
+                <div class="card">
+                    <img src="{{ Storage::url('akademik/pusat_informasi/' . $informasi->thumbnail) }}" class="card-img-top" alt="thumbnail">
+                    <div class="card-body">
+                        <p class="card-text"><small class="text-muted">Last updated {{ $informasi->updated_at->format('Y-m-d') }}</small></p>
+                        <h5 class="card-title">
+                            <a href="{{ route('informationcenter.detail', $informasi->id) }}">{{ \Illuminate\Support\Str::limit($informasi->title, 100) }}</a>
+                        </h5>
+                        {{-- <p class="card-text">{!! \Illuminate\Support\Str::limit($informasi->description, 10, '...') !!}</p> --}}
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="assets/thumbnails-blog.png" class="card-img-top" alt="thumbnail">
-                <div class="card-body">
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    <h5 class="card-title">
-                        <a href="">Card Title</a>
-                    </h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="assets/thumbnails-blog.png" class="card-img-top" alt="thumbnail">
-                <div class="card-body">
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    <h5 class="card-title">
-                        <a href="">Card Title</a>
-                    </h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
     <br>
     <p class="text-end"><a href="/informationcenter"><b>Lihat semua informasi</b></a></p>
