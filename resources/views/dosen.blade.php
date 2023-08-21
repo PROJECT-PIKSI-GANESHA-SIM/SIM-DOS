24018f5f9fa2deb52f373d04ca79f331820bbba4
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-08-21 17:58:16 +0700

    fix card-body in dosen page

--- resources/views/dosen.blade.php
+++ resources/views/dosen.blade.php
@@ -1,11 +1,12 @@
 <style>
     .card {
         transition: border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-        border-width: 2px; /* Nilai border lebih tebal */
+        border-width: 2px;
     }
+
     .card:hover {
         border-color: #8A00B9;
-        box-shadow: 0 8px 16px rgba(138, 0, 185, 0.3); /* Nilai bayangan lebih tebal */
+        box-shadow: 0 8px 16px rgba(138, 0, 185, 0.3);
     }
 </style>
 @include('layouts.partials.navbar')
@@ -22,14 +23,14 @@
         @foreach ($users_dosen as $dosen)
         <div class="col" id="myCard">
             <div class="card">
-                <img src="{{ Storage::url('dosen/profile/' . $dosen->image) }}" class="card-img-top" alt="thumbnail">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="{{ route('alldosen.detail',$dosen->id) }}"> {{ $dosen->name }}</a>
-                    </h5>
-                    <p class="card-text">{{ $dosen->nidn }}</p>
-                    <p class="card-text">{{ $dosen->program_studi }}</p>
-                </div>
+                <a href="{{ route('alldosen.detail', $dosen->id) }}">
+                    <img src="{{ Storage::url('dosen/profile/' . $dosen->image) }}" class="card-img-top" alt="thumbnail">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $dosen->name }}</h5>
+                        <p class="card-text">{{ $dosen->nidn }}</p>
+                        <p class="card-text">{{ $dosen->program_studi }}</p>
+                    </div>
+                </a>
             </div>
         </div>
         @endforeach
