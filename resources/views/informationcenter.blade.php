589d66ad282b1165866237ecf208bf07c15314b3
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-08-21 15:01:01 +0700

    fix hover card

--- resources/views/informationcenter.blade.php
+++ resources/views/informationcenter.blade.php
@@ -11,6 +11,19 @@
         display: flex;
         flex-direction: column;
         height: 100%;
+        border: 3px solid transparent;
+        /* Menambahkan border pada card dengan ketebalan 3px */
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        /* Efek shadow (bayangan) pada card */
+        transition: border-color 0.3s, box-shadow 0.3s;
+        /* Efek transisi untuk perubahan warna border dan shadow */
+    }
+
+    .card:hover {
+        border-color: #8A00B9;
+        /* Warna ungu untuk border ketika dihover */
+        box-shadow: 0 6px 8px rgba(0, 0, 0, 0.2);
+        /* Efek shadow lebih kuat saat dihover */
     }
 
     .row-cols-md-3 .col {
