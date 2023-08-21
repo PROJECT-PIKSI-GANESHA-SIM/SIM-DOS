6ce33a60f00e4f203d2fbe7c81ae81519a42d262
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-08-21 14:39:21 +0700

    fix detail information center page

--- resources/views/detail_informationcenter.blade.php
+++ resources/views/detail_informationcenter.blade.php
@@ -9,28 +9,27 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="p-4">
-        <a href="/informationcenter">
-            <i class="fa fa-arrow-left me-2"></i>
-            Kembali
-        </a>
+<div class="p-4">
+    <a href="/informationcenter">
+        <i class="fa fa-arrow-left me-2"></i>
+        Kembali
+    </a>
+</div>
+<div class="row mx-0 py-5">
+    <div class="col-md-5 px-4">
+        <img src="{{ Storage::url('akademik/pusat_informasi/' . $pusat_informasi->thumbnail) }}" class="img-fluid" alt="Thumbnail">
     </div>
-    <div class="row mx-0 py-5">
-        <div class="col-md-5 px-4">
-            <img src="{{ Storage::url('akademik/pusat_informasi/' . $pusat_informasi->thumbnail) }}" class="img-fluid" alt="Thumbnails">
-        </div>
 
-        <div class="col-md-7 px-4">
-            <h1 class="blog-item__title">{{ $pusat_informasi->title }}</h1>
-            <div class="mb-2 text-muted">{{ $pusat_informasi->updated_at->format('Y-m-d') }}</div>
+    <div class="col-md-7 px-4">
+        <h1 class="blog-item__title">{{ $pusat_informasi->title }}</h1>
+        <div class="mb-2 text-muted">{{ $pusat_informasi->updated_at->format('Y-m-d') }}</div>
+    </div>
+</div>
+<div class="row mx-0 py-5">
+    <div class="col-md-10 mx-auto p-4">
+        <div class="blog-content lh-lg">
+            {!! $pusat_informasi->description !!}
         </div>
     </div>
-    </div>
-    <div class="row mx-0 py-5">
-        <div class="col-md-10 mx-auto p-4">
-            <div class="blog-content lh-lg">
-                {!! $pusat_informasi->description !!}
-            </div>
-        </div>
-    </div>
+</div>
 @endsection
