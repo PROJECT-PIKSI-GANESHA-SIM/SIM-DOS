1328204ea53adef533fcb5b3d328accfd2e57b65
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-08-21 18:05:02 +0700

    clean code

--- resources/views/detail_informationcenter.blade.php
+++ resources/views/detail_informationcenter.blade.php
@@ -9,30 +9,31 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="p-4">
-    <a href="/informationcenter">
-        <i class="fa fa-arrow-left me-2"></i>
-        Kembali
-    </a>
-</div>
-<div class="row mx-0 py-5">
-    <div class="col-md-5 px-4">
-        <img src="{{ Storage::url('akademik/pusat_informasi/' . $pusat_informasi->thumbnail) }}" class="img-fluid" alt="Thumbnail">
+    <div class="p-4">
+        <a href="/informationcenter">
+            <i class="fa fa-arrow-left me-2"></i>
+            Kembali
+        </a>
     </div>
+    <div class="row mx-0 py-5">
+        <div class="col-md-5 px-4">
+            <img src="{{ Storage::url('akademik/pusat_informasi/' . $pusat_informasi->thumbnail) }}" class="img-fluid"
+                alt="Thumbnail">
+        </div>
 
-    <div class="col-md-7 px-4">
-        <h1 class="blog-item__title">{{ $pusat_informasi->title }}</h1>
-        <div class="mb-2 text-muted">{{ $pusat_informasi->updated_at->format('Y-m-d') }}</div>
-    </div>
-</div>
-<div class="row mx-0 py-5">
-    <div class="col-md-10 mx-auto p-4">
-        <div class="blog-content lh-lg">
-            {!! $pusat_informasi->description !!}
-            <br>
-            Salam,<br>
-            <b>Politeknik Piksi Ganesha</b>
+        <div class="col-md-7 px-4">
+            <h1 class="blog-item__title">{{ $pusat_informasi->title }}</h1>
+            <div class="mb-2 text-muted">{{ $pusat_informasi->updated_at->format('Y-m-d') }}</div>
         </div>
     </div>
-</div>
+    <div class="row mx-0 py-5">
+        <div class="col-md-10 mx-auto p-4">
+            <div class="blog-content lh-lg">
+                {!! $pusat_informasi->description !!}
+                <br>
+                Salam,<br>
+                <b>Politeknik Piksi Ganesha</b>
+            </div>
+        </div>
+    </div>
 @endsection
