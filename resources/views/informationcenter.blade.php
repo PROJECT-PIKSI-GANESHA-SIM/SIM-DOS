39f8af15bbb3868b2d7f25125753b592652b7e5b
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-09-01 15:08:23 +0700

    fixing too responsive and 3 cards

--- resources/views/informationcenter.blade.php
+++ resources/views/informationcenter.blade.php
@@ -5,6 +5,7 @@
         background-image: url('assets/patternpad-pusatinformasi.svg');
         background-repeat: no-repeat;
     }
+
     .card {
         display: flex;
         flex-direction: column;
@@ -13,10 +14,12 @@
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
         transition: border-color 0.3s, box-shadow 0.3s;
     }
+
     .card:hover {
         border-color: #8A00B9;
         box-shadow: 0 6px 8px rgba(0, 0, 0, 0.2);
     }
+
     .row-cols-md-3 .col {
         display: flex;
         flex: 1;
@@ -29,7 +32,7 @@
     </div>
     <div class="row row-cols-1 row-cols-md-3 g-4">
         @foreach ($pusat_informasi as $informasi)
-            <div class="col">
+            <div class="col-md-4 col-sm-12">
                 <div class="card">
                     <a href="{{ route('informationcenter.detail', $informasi->id) }}">
                         <img src="{{ Storage::url('akademik/pusat_informasi/' . $informasi->thumbnail) }}"
