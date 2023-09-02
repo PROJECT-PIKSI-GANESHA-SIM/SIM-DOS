5ebd044a67f483ac2c31394ca33aa2cfd73aed13
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-09-02 22:11:05 +0700

    add favicon in auth blade (when login page)

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>SIM DOSEN</title>
+    <link rel="icon" type="image/x-icon" href="assets/PIKSI.png">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
