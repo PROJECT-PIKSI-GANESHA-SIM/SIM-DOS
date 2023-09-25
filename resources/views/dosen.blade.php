2b2fe6aea5e5de5614831ffaef7cff709a205ec5
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-09-25 16:48:05 +0700

    rename Prodi to Program Studi in dosen page

--- resources/views/dosen.blade.php
+++ resources/views/dosen.blade.php
@@ -19,7 +19,7 @@
     <h1 class="text-4xl text-gray-700 mb-2 font-weight-bold py-3">
         <center>Daftar Profil Dosen</center>
     </h1>
-    <p class="py-3">Masukkan Nama Dosen, NIDN atau Prodi</p>
+    <p class="py-3">Masukkan Nama Dosen, NIDN atau Program Studi</p>
     <input class="form-control" id="myInput" type="text" placeholder="Cari..">
     <br>
     <div class="row row-cols-1 row-cols-md-4 g-4">
