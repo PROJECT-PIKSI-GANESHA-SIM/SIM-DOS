d8ddb411310425c503c74ca9d421bd6ffb809e5a
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2024-10-07 23:04:52 +0700

    fixing blade

--- resources/views/layouts/akademik-dashboard.blade.php
+++ resources/views/layouts/akademik-dashboard.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<!-- 
+<!--
 * Bootstrap Simple Admin Template
 * Version: 2.1
 * Author: Alexis Luna
@@ -11,15 +11,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>SIM DOSEN</title>
+    <title>{{ $title }} | SIM DOSEN</title>
     <link rel="icon" href="{{ asset('assets/PIKSI.png') }}">
-    <link href="/assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
-    <link href="/assets/vendor/fontawesome/css/solid.min.css" rel="stylesheet">
-    <link href="/assets/vendor/fontawesome/css/brands.min.css" rel="stylesheet">
-    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/css/master.css" rel="stylesheet">
+    <link href="{{ assets('/assets/vendor/fontawesome/css/fontawesome.min.css') }}" rel="stylesheet">
+    <link href="{{ assets('/assets/vendor/fontawesome/css/solid.min.css') }}" rel="stylesheet">
+    <link href="{{ assets('/assets/vendor/fontawesome/css/brands.min.css') }}" rel="stylesheet">
+    <link href="{{ assets('/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ assets('/assets/css/master.css') }}" rel="stylesheet">
     {{-- <link href="/css/style.css" rel="stylesheet"> --}}
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ assets('/css/app.css') }}" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/38.1.1/classic/ckeditor.js"></script>
     <!-- <link href="assets/vendor/flagiconcss/css/flag-icon.min.css" rel="stylesheet"> -->
@@ -51,11 +51,11 @@
     <script src="assets/js/dashboard-charts.js"></script> -->
     <script src="/assets/js/script.js"></script>
     <script>
-         ClassicEditor
-            .create( document.querySelector( '#description' ) )
-            .catch( error => {
-                console.error( error );
-            } );
+        ClassicEditor
+            .create(document.querySelector('#description'))
+            .catch(error => {
+                console.error(error);
+            });
     </script>
 </body>
 
